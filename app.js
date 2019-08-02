3de5a761bbdabf406ef6fe86dd0cd315490b2146
TatianaYudakova <36727075+TatianaYudakova@users.noreply.github.com>
2019-08-02 09:39:50 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var data;
+var json;
+var weatherForecastArray = [];
+var currentweatherForecastArray = [];
+var change = 0;
+var rightButton = document.getElementById('right-button');
+var leftButton = document.getElementById('left-button');
+
+class Property {
+	constructor(description, styleClass) {
+		this.description = description;
+		this.styleClass = styleClass;
+	}
+}
 
 window.onload = function() {
 	var xhr = new XMLHttpRequest();
@@ -10,97 +22,144 @@ window.onload = function() {
 	if (xhr.status != 200) {
 	  alert( xhr.status + ': ' + xhr.statusText ); 
 	} else {
-	 insertData(xhr.responseText);
+	 fillWeatherForecastArray(xhr.responseText);
+	 insertWeather(currentweatherForecastArray);
+	 checkedDisabledButtons();
 	}
 };
 
-function insertData(data) {
-	var json = JSON.parse(data);
+function fillWeatherForecastArray(data) {
+	json = JSON.parse(data);
+	let fixCount = findNewDate(json);
+	let currentCount = 0;
+	weatherForecastArray.push(getWeatherForecast(currentCount));
 
-	var fixCount = findNewDate(json);
-	var changeCount;
+	let i = 0; 
+	currentCount = fixCount + i*8 + 4;
 
-	var divs = document.getElementsByClassName('blok-of-day');
+	while (currentCount < json.list.length) {
+		weatherForecastArray.push(getWeatherForecast(currentCount));
+		i++;
+		currentCount = fixCount + i*8 + 4;
+	}
 
-	for (var i = 0; i < divs.length; i++) {
-		var day = document.createElement('li');
-		var date = document.createElement('li');
-		var icon = document.createElement('li');
-		var iconCode = document.createElement('img');
-		var dayTemp = document.createElement('li');
-		var nightTemp = document.createElement('li');
-		var weather = document.createElement('li');
-		var wind = document.createElement('li');
+	for (i = 0; i < 4; i++) {
+		currentweatherForecastArray.push(weatherForecastArray[i]);
+	}
+}
 
-		date.className = 'date-of-day';
-		iconCode.className = 'icon';
-		nightTemp.className = 'temperature-night';
-		weather.className = 'weather';
-		wind.className = 'wind';
+function getWeatherForecast(changeCount) {
+	let weatherForecast = {};
+	let dateDate = new Date(json.list[changeCount].dt * 1000);
 
-		if (i == 0) {
-			changeCount = 0;
+	if (changeCount == 0) {
+		weatherForecast.day = new Property('Сегодня', '');
+		weatherForecast.dayTemp = new Property(Math.round(json.list[changeCount].main.temp) + ' °C', 'temperature-today');
+	} else {
+		weatherForecast.day = new Property(upperFirstCharacter(dateDate.toLocaleString("ru", {weekday: 'long'})), '');
+		weatherForecast.dayTemp = new Property('Днем ' + Math.round(json.list[changeCount].main.temp) + ' °C', 'temperature-day');
+		weatherForecast.nightTemp = new Property('Ночью ' + Math.round(json.list[changeCount - 4].main.temp) + ' °C', 'temperature-night');
+	}
+	weatherForecast.date = new Property(dateDate.getUTCDate() + ' ' + replaceLastCharacter(dateDate), 'date-of-day');
+	weatherForecast.iconCode = new Property('http://openweathermap.org/img/w/' + json.list[changeCount].weather[0].icon + '.png', 'icon');
+	weatherForecast.weather = new Property(json.list[changeCount].weather[0].description, 'weather');
+	weatherForecast.wind = new Property('Ветер ' + json.list[changeCount].wind.speed + ' м/с', 'wind');
+	return weatherForecast;
+}
 
-			day.innerHTML = 'Сегодня';
-			dayTemp.innerHTML = Math.round(json.list[0].main.temp) + ' °C';
+function insertWeather(data) {
+	let table = document.getElementById('table');
+	let tr = document.createElement('tr');
+	tr.id = 'tr';
+	table.appendChild(tr);
+	console.log(data);
+	for (let i = 0; i < 4; i++) {
+		let day = document.createElement('li');
+		let date = document.createElement('li');
+		let icon = document.createElement('li');
+		let iconCode = document.createElement('img');
+		let dayTemp = document.createElement('li');
+		let nightTemp = document.createElement('li');
+		let weather = document.createElement('li');
+		let wind = document.createElement('li');
 
-			dayTemp.className = 'temperature-today';
-		} else {
-			changeCount = fixCount + (i - 1)*8 + 4;
+		day.innerHTML = data[i].day.description;
+		dayTemp.innerHTML = data[i].dayTemp.description;
+		dayTemp.className = data[i].dayTemp.styleClass;
 
-			dayTemp.innerHTML = 'Днем ' + Math.round(json.list[changeCount].main.temp) + ' °C';
-			nightTemp.innerHTML = 'Ночью ' + Math.round(json.list[changeCount - 4].main.temp) + ' °C';
-
-			dayTemp.className = 'temperature-day';
+		if (data[i].day.description !== 'Сегодня') {
+			nightTemp.innerHTML = data[i].nightTemp.description;
+			nightTemp.className = data[i].nightTemp.styleClass;
 		}
 
-		dateDate = new Date(json.list[changeCount].dt * 1000);
-		date.innerHTML = dateDate.getUTCDate() + ' ' + replaceLastCharacter(dateDate);
+		date.innerHTML = data[i].date.description;
+		date.className = data[i].date.styleClass;
 
-		iconCode.src = "http://openweathermap.org/img/w/" + json.list[changeCount].weather[0].icon + ".png";
+		iconCode.src = data[i].iconCode.description;
+		iconCode.className = data[i].iconCode.styleClass;
 		icon.appendChild(iconCode);
 
-		weather.innerHTML = json.list[changeCount].weather[0].description;
-		wind.innerHTML = 'Ветер ' + json.list[changeCount].wind.speed + ' м/с';
+		weather.innerHTML = data[i].weather.description;
+		weather.className = data[i].weather.styleClass;
 
-		if (i == 0) {
-			addListToday(day, date, icon, dayTemp, weather, wind, divs[0]);
+		wind.innerHTML = data[i].wind.description;
+		wind.className = data[i].wind.styleClass;
+
+		if (data[i].day.description === 'Сегодня') {
+			addListToday(day, date, icon, dayTemp, weather, wind, tr);
 		} else {
-			day.innerHTML = upperFirstCharacter(dateDate.toLocaleString("ru", {weekday: 'long'}));
-			addList(day, date, icon, dayTemp, nightTemp, weather, wind, divs[i]);
+			addList(day, date, icon, dayTemp, nightTemp, weather, wind, tr);
 		}
 	}
 }
 
-function addList(day, date, icon, dayTemp, nightTemp, weather, wind, div) {
-	var ul = div.getElementsByTagName('ul');
+function addList(day, date, icon, dayTemp, nightTemp, weather, wind, tr) {
+	let td = document.createElement('td');
 
-	ul[0].appendChild(day);
-	ul[0].appendChild(date);
-	ul[0].appendChild(icon);
-	ul[0].appendChild(dayTemp);
-	ul[0].appendChild(nightTemp);
-	ul[0].appendChild(weather);
-	ul[0].appendChild(wind);
+	let div = document.createElement('div');
+	div.className = 'bloсk-of-day';
+
+	let ul = document.createElement('ul');
+
+	ul.appendChild(day);
+	ul.appendChild(date);
+	ul.appendChild(icon);
+	ul.appendChild(dayTemp);
+	ul.appendChild(nightTemp);
+	ul.appendChild(weather);
+	ul.appendChild(wind);
+
+	div.appendChild(ul);
+	td.appendChild(div);
+	tr.appendChild(td);
 }
 
-function addListToday(day, date, icon, temp, weather, wind, div) {
-	var ul = div.getElementsByTagName('ul');
+function addListToday(day, date, icon, temp, weather, wind, tr) {
+	let td = document.createElement('td');
 
-	ul[0].appendChild(day);
-	ul[0].appendChild(date);
-	ul[0].appendChild(icon);
-	ul[0].appendChild(temp);
-	ul[0].appendChild(weather);
-	ul[0].appendChild(wind);
+	let div = document.createElement('div');
+	div.className = 'bloсk-of-day';
+
+	let ul = document.createElement('ul');
+
+	ul.appendChild(day);
+	ul.appendChild(date);
+	ul.appendChild(icon);
+	ul.appendChild(temp);
+	ul.appendChild(weather);
+	ul.appendChild(wind);
+
+	div.appendChild(ul);
+	td.appendChild(div);
+	tr.appendChild(td);
 }
 
 function findNewDate(json) {
-	var i = 1;
-	var date1 = new Date(json.list[0].dt * 1000).getUTCDate();
-	var date2;
+	let i = 1;
+	let date1 = new Date(json.list[0].dt * 1000);
+	let date2;
 	while(true) {
-		date2 = new Date(json.list[i].dt * 1000).getUTCDate();
+		date2 = new Date(json.list[i].dt * 1000);
 		if (date1 < date2) {
 			break;
 		}
@@ -114,13 +173,62 @@ function upperFirstCharacter(str) {
 }
 
 function replaceLastCharacter(date) {
-	var numMonth = date.getMonth();
-	var month = date.toLocaleString("ru", {month: 'long'});
-	if (numMonth !== 2 || numMonth !== 7) {
+	let numMonth = date.getUTCMonth();
+	let month = date.toLocaleString("ru", {month: 'long'});
+	if (numMonth !== 2 && numMonth !== 7) {
 		month = month.substring(0, month.length - 1) + 'я';
 	} else {
 		month += 'a';
 	}
 
 	return month;
+}
+
+function changeButtons(buttonId) {
+	
+	if (buttonId == 'left-button') {
+		change--;
+		updateWeather('left');
+	} else if (buttonId == 'right-button') {
+		change++;
+		updateWeather('right');
+	}
+
+	checkedDisabledButtons();
+}
+
+function checkedDisabledButtons() {
+	if (change == 0) {
+		leftButton.disabled = true;
+		leftButton.classList.add('disabled-button');
+		leftButton.classList.remove('active-button');
+	} else {
+		leftButton.disabled = false;
+		leftButton.classList.add('active-button');
+		leftButton.classList.remove('disabled-button');
+	}
+
+	if (change == weatherForecastArray.length - 4) {
+		rightButton.disabled = true;
+		rightButton.classList.add('disabled-button');
+		rightButton.classList.remove('active-button');
+	} else {
+		rightButton.disabled = false;
+		rightButton.classList.add('active-button');
+		rightButton.classList.remove('disabled-button');
+	}
+}
+
+function updateWeather(direction) {
+	if (direction == 'right') {
+		currentweatherForecastArray.shift();
+		currentweatherForecastArray.push(weatherForecastArray[change+3]);
+		document.getElementById('tr').remove();
+		insertWeather(currentweatherForecastArray);
+	} else {
+		currentweatherForecastArray.pop();
+		currentweatherForecastArray.unshift(weatherForecastArray[change]);
+		document.getElementById('tr').remove();
+		insertWeather(currentweatherForecastArray);
+	}
 }
